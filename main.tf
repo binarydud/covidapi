ff3f3db83c6d6c2db27770f156472b814c15ab6b
Matt George <mgeorge@gmail.com>
2020-06-13 09:45:59 -0600

    building resources in 0.12.26

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = "0.12.26"
   backend "s3" {
     bucket = "atlas-terraform-state-dev"
     key    = "covid-tf.state"
@@ -33,7 +34,22 @@ resource "aws_dynamodb_table" "covid-us-table" {
   }
 }
 resource "aws_iam_role" "cacheRole" {
-  name               = "cacheRole"
+  name               = "covidCacheRole"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Action": "sts:AssumeRole",
+    "Principal": {
+      "Service": "lambda.amazonaws.com"
+    },
+    "Effect": "Allow"
+  }
+}
+POLICY
+}
+resource "aws_iam_role" "apiRole" {
+  name               = "covidApiRole"
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -57,6 +73,16 @@ resource "aws_lambda_function" "covidCache" {
   memory_size      = 128
   timeout          = 1
 }
+resource "aws_lambda_function" "covidAPIv2" {
+  function_name    = "covidAPIv2"
+  filename         = "dist/api.zip"
+  handler          = "api"
+  source_code_hash = filebase64("dist/api.zip")
+  role             = "${aws_iam_role.apiRole.arn}"
+  runtime          = "go1.x"
+  memory_size      = 128
+  timeout          = 1
+}
 resource "aws_apigatewayv2_api" "covidAPI" {
   name          = "covid-api"
   protocol_type = "HTTP"
@@ -66,6 +92,6 @@ resource "aws_apigatewayv2_integration" "base" {
   integration_type       = "AWS_PROXY"
   description            = "Lambda example"
   integration_method     = "POST"
-  integration_uri        = "${aws_lambda_function.example.invoke_arn}"
-  payload_format_version = 1.0
+  integration_uri        = "${aws_lambda_function.covidAPIv2.invoke_arn}"
+  payload_format_version = "1.0"
 }
